2e9540668398d250fe3b88a739ed6faaf2ac3090
Lyrcy <limit1219c@gmail.com>
2022-05-14 12:13:28 +0800

    update dip721 card list

--- frontend/component/dashboard/Dip721IdleCardList.jsx
+++ frontend/component/dashboard/Dip721IdleCardList.jsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import Dip721Card from './Dip721Card'
 import AppContext from "../../AppContext"
+import { Principal } from "@dfinity/principal"
+import { dip721 } from "canisters/dip721"
 
 export default function Dip721IdleCardList () {
     const { userPrincipal } = React.useContext(AppContext)
     const [nfts, setNfts] = React.useState([])
 
+    async function fetch (userPrincipal) {
+        let tokenIds = await dip721.getTokenIdsForUserDip721(userPrincipal)
+        return (await Promise.all(tokenIds.map(id => dip721.getMetadataDip721(id))))
+            .map(e => adapter(e))
+    }
+    function adapter (metadataDip721) {
+        if (!metadataDip721.Ok) return null
+        let nft = {}
+        let metadata = metadataDip721.Ok[0];
+        nft.data = metadata.data
+        nft.purpose = metadata.purpose
+        metadata.key_val_data.forEach(e => {
+            nft[e.key] = e.val.TextContent
+        })
+        return nft
+    }
+    React.useEffect(() => {
+
+        userPrincipal = Principal.fromText("t5f3o-4jc2c-kedu6-zkclt-jygbq-llpj7-o5sjc-nlwal-l6mis-zk4mq-jae")
+            (async () => {
+                let nfts = await fetch(userPrincipal)
+                setIdleNfts(nfts)
+                console.log("data:" + JSON.stringify(nfts))
+            })()
+
+
+    }, [userPrincipal])
+
     return (
         <div className="flex flex-row flex-wrap">
             {
-                idleNfts.map((e, index) => (<Dip721Card key={index} location={e.location}></Dip721Card>))
+                nfts.map((e, index) => (<Dip721Card key={index} location={e.location}></Dip721Card>))
             }
         </div>
     )
--- frontend/component/dashboard/Idle.jsx
+++ frontend/component/dashboard/Idle.jsx
@@ -1,48 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import { marketplace } from "canisters/marketplace"
-import { dip721 } from "canisters/dip721"
 
-import { Principal } from "@dfinity/principal"
+
+
 import NftCard from "./Dip721Card"
 import Dip721IdleCardList from "./Dip721IdleCardList"
 
 export default function IdleList () {
   
-  
-  //setUserPrincipal("t5f3o-4jc2c-kedu6-zkclt-jygbq-llpj7-o5sjc-nlwal-l6mis-zk4mq-jae")
 
-  async function fetchNftFromDip721 (userPrincipal) {
-    userPrincipal = Principal.fromText("t5f3o-4jc2c-kedu6-zkclt-jygbq-llpj7-o5sjc-nlwal-l6mis-zk4mq-jae")
-    console.log("user principal is: " + userPrincipal.toText())
-    let uniDip721Nfts = [];
-    let tokenIds = await dip721.getTokenIdsForUserDip721(userPrincipal)
-    return (await Promise.all(tokenIds.map(id => dip721.getMetadataDip721(id))))
-      .map(e => dip721Adapter(e))
-  }
-  function dip721Adapter (metadataDip721) {
 
-    if (!metadataDip721.Ok) return null
-    let uniData = {}
-    let metadata = metadataDip721.Ok[0];
-    uniData.data = metadata.data
-    uniData.purpose = metadata.purpose
-    metadata.key_val_data.forEach(e => {
-      uniData[e.key] = e.val.TextContent
-    })
-    return uniData
-  }
-  React.useEffect(() => {
-    //TODO, 针对每一个第三方nft项目实现一个子类,来做差异化处理
-    if (true) {
-      (async () => {
-        let nfts = await fetchNftFromDip721(userPrincipal)
-        setIdleNfts(nfts)
-        console.log("data:" + JSON.stringify(nfts))
-      })()
-    }
 
-  }, [userPrincipal])
+
 
 
   return (
