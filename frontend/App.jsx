298f914efb5c63d8290efcb4703338dc4d729f30
Lyrcy <limit1219c@gmail.com>
2022-06-02 11:58:04 +0800

    add canister ids

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -10,9 +10,17 @@ import Footer from "./component/Footer"
 function App () {
   const [connected, setConnected] = React.useState(false)
   const [userPrincipal, setUserPrincipal] = React.useState()
+  const nftCanisterId = "rrkah-fqaaa-aaaaa-aaaaq-cai"
+
+  const canisters = {
+    nftCanisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
+    marketplaceCanisterId: "rkp4c-7iaaa-aaaaa-aaaca-cai",
+    sharingCanisterId: "rno2w-sqaaa-aaaaa-aaacq-cai",
+    ledgerCanisterId: "qaa6y-5yaaa-aaaaa-aaafa-cai",
+  }
   return (
     <AppContext.Provider
-      value={{ connected, setConnected, userPrincipal, setUserPrincipal }}
+      value={{ connected, setConnected, userPrincipal, setUserPrincipal, canisters }}
     >
       <div className="lg:container flex flex-col flex-nowrap mx-auto">
         <TopNav></TopNav>
