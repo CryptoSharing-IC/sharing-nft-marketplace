079ab6f058f80c5a885bd5db0e535790fbabb201
Lyrcy <limit1219c@gmail.com>
2022-06-02 16:57:30 +0800

    make a function

--- frontend/component/dashboard/ListingFlow.jsx
+++ frontend/component/dashboard/ListingFlow.jsx
@@ -6,6 +6,9 @@ import StakeStep from './StakeStep';
 
 export default function ListingFlow (props) {
     let [currentStep, setCurrentStep] = React.useState(1);
+    let nextStep = () => {
+        setCurrentStep(currentStep++ % 4 + 1)
+    }
     return (
         <>
             <input type="checkbox" id="listing-step" className="modal-toggle" />
@@ -13,10 +16,10 @@ export default function ListingFlow (props) {
                 <div className="modal-box">
                     {
                         {
-                            1: <LengingStep nftData={props.nftData} setCurrentStep={setCurrentStep}></LengingStep>,
-                            2: <StakeStep nftData={props.nftData} setCurrentStep={setCurrentStep}></StakeStep>,
-                            3: <MintStep nftData={props.nftData} setCurrentStep={setCurrentStep}></MintStep>,
-                            4: <FinishListingStep nftData={props.nftData}></FinishListingStep>
+                            1: <LengingStep nftData={props.nftData} nextStep={nextStep}></LengingStep>,
+                            2: <StakeStep nftData={props.nftData} nextStep={nextStep}></StakeStep>,
+                            3: <MintStep nftData={props.nftData} nextStep={nextStep}></MintStep>,
+                            4: <FinishListingStep nftData={props.nftData} nextStep={nextStep}></FinishListingStep>
                         }[currentStep]
                     }
                 </div>
