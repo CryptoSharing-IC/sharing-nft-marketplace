952b73f8752bcd27dade90422df0f1613a29da67
Lyrcy <limit1219c@gmail.com>
2022-06-02 16:55:29 +0800

    add canister ids

---
+++ frontend/canister_ids.js
@@ -0,0 +1,6 @@
+export default {
+    nftCanisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
+    marketplaceCanisterId: "rkp4c-7iaaa-aaaaa-aaaca-cai",
+    sharingCanisterId: "rno2w-sqaaa-aaaaa-aaacq-cai",
+    ledgerCanisterId: "qaa6y-5yaaa-aaaaa-aaafa-cai",
+}
